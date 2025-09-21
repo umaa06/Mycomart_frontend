79a2288606107063f688309758aa601e177ceace
MadhushikaN <nadeeshanim46@gmail.com>
2025-09-21 17:44:44 +0530

    link color

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -74,15 +74,15 @@ export default function Login() {
   return (
       <div className="bg-image flex items-center justify-center min-h-screen bg-gray-100 font-sans">
       <div className="bg-white rounded-2xl shadow-lg p-10 max-w-md w-full">
-        <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-6">
-          Mushroom Sales System
-        </h2>
         <Image
         src="/mycomart_logo.png"
         alt="MYCOMART Logo"
-        width={96}
-        height={96}
+        width={120}
+        height={100}
         className="mx-auto mb-6 w-24 h-auto"/>
+        <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-6">
+          Mushroom Sales System
+        </h2>
 
         <p className="text-xl font-semibold text-gray-700 text-center mb-8">
           Login to your account
@@ -155,7 +155,7 @@ export default function Login() {
           <div className="text-center text-sm">
             <Link
               href="/forgot-password"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
+              className="font-medium text-[#402101] hover:text-[#7B3F00]"
             >
               Forgot your password?
             </Link>
