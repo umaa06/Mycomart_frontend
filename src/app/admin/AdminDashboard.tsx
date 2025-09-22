8193ac4b6c9dac43eaa76f0aa78850fa4a7a190c
anduniInduja <anduniinduja@gmail.com>
2025-09-23 00:40:57 +0530

    link error fix

--- src/app/admin/AdminDashboard.tsx
+++ src/app/admin/AdminDashboard.tsx
@@ -73,7 +73,7 @@ const AdminDashboard = () => {
     { href: '/admin/admin-dashboard', label: 'Dashboard', active: true },
     { href: '/admin/manage-shops', label: 'Manage Shops', active: false },
     { href: '/admin/manage-delivery-people', label: 'Manage delivery People', active: false },
-    { href: 'admin/view-all-orders', label: 'View All Orders', active: false },
+    { href: '/admin/view-all-orders', label: 'View All Orders', active: false },
     { href: '/admin/report', label: 'Reports', active: false },
     { href: '/admin/setting', label: 'Settings', active: false },
   ];
