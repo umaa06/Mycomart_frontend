65a101e941b324adb0126650f51b52d355322b60
SandaruAkalanka <a.sandaru@gtngroup.com>
2025-09-22 13:58:54 +0530

    issue fix

--- src/app/types/deliveryRecord.ts
+++ src/app/types/deliveryRecord.ts
@@ -1,10 +1,10 @@
 type DeliveryRecord = {
     order_id: number | null;
-    order_date: string | null;
+    order_date: string;
     shop_name: string | null;
     shop_address: string | null;
     order_items_summary: string | null;
-    total_amount: number | null;
+    total_amount: number;
     expected_delivery_date: string | null;
     delivery_date: string | null;
     status: string | null;
